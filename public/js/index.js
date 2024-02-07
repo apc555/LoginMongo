d2598473ddb5d1434d7185476495fd36cbc1fd72
afd 23-24 tardes <afdt@alupostal4.local>
2024-02-07 21:30:47 +0100

    cambios de israel do 07/08/2024

--- public/js/index.js
+++ public/js/index.js
@@ -4,26 +4,11 @@ const formLogin = document.getElementById("loginForm")
 
 
 
-envio.addEventListener ("click", (e) =>
+envio.addEventListener ("click",async (e) =>
 {
     e.preventDefault();
 
-    const usuario = formLogin.usuario.value;
-    const password = formLogin.password.value;
-
-    let datos = new FormData(loginForm);
-
-    for(let [name, value] of datos) {
-        console.log(`${name} = ${value}`);
-    }  
-
-    if (usuario === "user" && password === "1234") {
-        alert("Haz hecho login")
-    
-    }
-
-    else {
-        alert("Error de inicio")
-    }
+    let recibido = await fetch('/logueo',{method:'POST', body: new FormData(loginForm)})
+  
 })
 
--- server.js
+++ server.js
@@ -2,14 +2,20 @@ const express = require("express");
 const path = require("path");
 const cors = require("cors");
 const app = express(); 
-
+const fileUpload = require("express-fileupload");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors())
+app.use(fileUpload());
 
 const Static = path.join(__dirname, "public/");
 app.use(express.static(Static));
 
+app.post("/logueo",(req,res)=>{
+    console.log("datos recibidos en req.body: ",req.body)
+    console.log("datos recibidos en req.body.usuario: ",req.body.usuario)
+    console.log("datos recibidos en req.body.password: ",req.body.password)
+})
 app.listen(5000, function() {
     console.log("Server running");
   });
