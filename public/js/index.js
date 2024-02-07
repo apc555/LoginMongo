7651d5acf5e4238290f57d85ac72b8ab48513272
afd 23-24 tardes <afdt@alupostal4.local>
2024-02-07 19:54:22 +0100

    commit con if de login

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,29 @@
 console.log('hola')
 
+const formLogin = document.getElementById("loginForm")
+
+
+
 envio.addEventListener ("click", (e) =>
 {
     e.preventDefault();
+
+    const usuario = formLogin.usuario.value;
+    const password = formLogin.password.value;
 
     let datos = new FormData(loginForm);
 
     for(let [name, value] of datos) {
         console.log(`${name} = ${value}`);
+    }  
+
+    if (usuario === "user" && password === "1234") {
+        alert("Haz hecho login")
+    
     }
 
-    
+    else {
+        alert("Error de inicio")
+    }
 })
+
